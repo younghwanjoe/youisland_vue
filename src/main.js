bb55c285866ae043adf6ccc34a0e9d1446057771
조영환 <hhgman2@gmail.com>
2020-05-16 10:45:13 +0900

    vue create add vue router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+window.axios = axios;
 
 Vue.config.productionTip = false
 
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount('#app') 
